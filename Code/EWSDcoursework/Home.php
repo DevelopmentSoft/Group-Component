33d101ef7b9ed1fa5a1e067869f82ba77717b8d6
Cchuan99 <78845364+Cchuan99@users.noreply.github.com>
2021-03-31 13:21:42 +0800

    Update Home.php

--- Code/EWSDcoursework/Home.php
+++ Code/EWSDcoursework/Home.php
@@ -20,62 +20,6 @@
 ?>
 <body>
     <!-- our -->
-<!--
-    <div id="txtarea" class="our">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="titlepage">
-                        <h2>Home</h2>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-12 margin_bottom">
-                    <div class="row">
-
-                        <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
-                            <div class="two-box">
-                                <figure><img style="width:300px;height:250px;" src="../EWSDcoursework/Layout/mainpage/images/gradurate.jpg" alt="#" /></figure>
-                            </div>
-                        </div>
-
-                        <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12">
-
-                            <div class="txtarea">
-                            <form>
-                                <div class="mb-3">
-                                    <label for="exampleInputEmail1" class="form-label">Information</label>
-                                    <input type="text" class="form-control" id="inputinfo" placeholder="Type" required>
-                                </div>
-                                <label for="Image" class="form-label">Image</label>
-                                <div class="custom-file">
-                                    <input type="file" class="custom-file-input" id="inputGroupFile04">
-                                    <label class="custom-file-label" for="inputGroupFile04">Choose file</label>
-                                </div>
-
-                                <label for="file" class="form-label">File</label>
-                                <div class="custom-file" style="margin: 20px 0">
-                                    <input type="file" class="custom-file-input" id="inputGroupFile04">
-                                    <label class="custom-file-label" for="inputGroupFile04">Choose file</label>
-                                </div>
-                                
-                                <div class="row" style="float:right;">
-                                <button type="button" class="btn btn-primary">Save Draft</button>
-                                <button style="margin-left:20px;" type="button" class="btn btn-success" onclick="">Submit</button>
-                                </div>
-
-                            </form>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-            </div>
-        </div>
-    </div>
-   
--->
      <div id="txtarea" class="our">
         <div class="container">
             <div class="row">
@@ -134,10 +78,68 @@
                     </div>
                 </div>
             </div>
+		 <div id="chart-container">
+                <canvas id="graphCanvas"></canvas>
+            </div>
         </div>
     </div>
     <!-- end our -->
 
 </body>
 
+<script src="../EWSDcoursework/Layout/mainpage/js/jquery-3.3.1.js"></script>
+<script src="../EWSDcoursework/Layout/mainpage/js/jquery-3.3.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+        showGraph();
+    });
+
+    function showGraph() {
+
+        var faculty = [];
+        var total = [];
+
+        $.ajax({
+            url: "chartdata.php",
+            type: "GET",
+            dataType: "json",
+            success: function(data) {
+
+                //======== Loop Value into Array ===========
+                for (var i in data) {
+                    faculty.push(data[i].Faculty);
+                    total.push(data[i].Total);
+                }
+                //======== Loop Value into Array End ===========
+
+                //========== Chart ==========
+                var chartdata = {
+                    labels: faculty,
+                    datasets: [{
+                        label: faculty,
+                        backgroundColor: '#49e2ff',
+                        borderColor: '#46d5f1',
+                        hoverBackgroundColor: '#CCCCCC',
+                        hoverBorderColor: '#666666',
+                        data: total
+                    }]
+                };
+
+                var graphTarget = $("#graphCanvas");
+
+                var barGraph = new Chart(graphTarget, {
+                    type: 'bar',
+                    data: chartdata
+                });
+                //========== End Chart ===========
+            },
+            error: function(data) {
+                console.log(data);
+                alert("Something wrong");
+            }
+        });
+    }
+</script>
 <?php include '../EWSDcoursework/footer.php' ?>
